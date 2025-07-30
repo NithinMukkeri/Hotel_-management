34a392e9c6fa343f9ac7c285c1c4f953a07417ce
mukkerinithin <mukkerinithin237@gmail.com>
2025-07-30 21:12:46 +0530

    Added project files

---
+++ src/test/java/com/Hotel/hotelManagement/HotelMamagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hotel.hotelManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMamagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
